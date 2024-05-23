dbb96d830fa7421da86fb607c7db55a039ff9821
Arjun Singh <singharjun.biz@gmail.com>
2024-05-23 18:39:49 +0530

    use ninja as the build tool

--- build.bat
+++ build.bat
@@ -1,20 +1,9 @@
 @echo off
 
-REM Install dependencies
-call ./install.bat
-
-REM Load the .env file
-for /f "delims== tokens=1,2" %%i in (.env) do set %%i=%%j
-
-REM Get the path of the bat file
-set BAT_FILE_PATH=%VCVARSALL_PATH%
-
-REM Execute the bat file
-call %BAT_FILE_PATH% x86
+REM Set the triplet environment variable for vcpkg
+SET VCPKG_TARGET_TRIPLET=x64-mingw-static
 
 REM Execute CMake
-set CC=cl
-set CXX=cl
 
-call cmake -S . -B ./build
-call cmake --build ./build --config Release
+call cmake --preset=default
+call cd build && ninja
